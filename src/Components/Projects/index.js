e7cb3f5b0bc2ae3f68e397cc14949bdf7b876450
Shemeer <shameerbem@gmail.com>
2023-11-13 15:14:41 +0530

    updated commit

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -14,9 +14,9 @@ function Project() {
     <div className='pt-10 mt-12'>
         <h1 className="text-3xl font-bold mb-4 text-center">Projects</h1>
         <div className='flex flex-wrap justify-center gap-8 mt-16'>
-            <Card image={<Image src={crypto} alt='cryp' />} title={"Coinwatchr App"} projectUrl={process.env.COINWATCHR_URL} repositoryUrl={process.env.COINWATCHR_REPO} techStacks={['REACT.JS','HTML', 'CSS', 'JAVASCRIPT', 'MATERIAL UI', 'FRAMER MOTION']}/>
-            <Card image={<Image src={trailervista} alt='net'/>} title={"Trailervista App"} projectUrl={process.env.TRAILERVISTA_URL} repositoryUrl={process.env.TRAILERVISTA_REPO} techStacks={['REACT.JS','HTML', 'CSS', 'JAVASCRIPT', 'MATERIAL UI', 'FIREBASE']}/>
-            <Card image={<Image src={shopping} alt='shop' />} title={"easyTodo App"} projectUrl={process.env.EASYTODO_URL} repositoryUrl={process.env.EASYTODO_REPO} techStacks={['REACT.JS','EXPRESS.JS','NODE.JS','MONGODB','HTML', 'CSS', 'JAVASCRIPT','MATERIAL UI']}/>
+            <Card image={<Image src={crypto} alt='cryp' />} title={"Coinwatchr App"} projectUrl={process.env.NEXT_PUBLIC_COINWATCHR_URL} repositoryUrl={process.env.NEXT_PUBLIC_COINWATCHR_REPO} techStacks={['REACT.JS','HTML', 'CSS', 'JAVASCRIPT', 'MATERIAL UI', 'FRAMER MOTION']}/>
+            <Card image={<Image src={trailervista} alt='net'/>} title={"Trailervista App"} projectUrl={process.env.NEXT_PUBLIC_TRAILERVISTA_URL} repositoryUrl={process.env.NEXT_PUBLIC_TRAILERVISTA_REPO} techStacks={['REACT.JS','HTML', 'CSS', 'JAVASCRIPT', 'MATERIAL UI', 'FIREBASE']}/>
+            <Card image={<Image src={shopping} alt='shop' />} title={"easyTodo App"} projectUrl={process.env.NEXT_PUBLIC_EASYTODO_URL} repositoryUrl={process.env.NEXT_PUBLIC_EASYTODO_REPO} techStacks={['REACT.JS','EXPRESS.JS','NODE.JS','MONGODB','HTML', 'CSS', 'JAVASCRIPT','MATERIAL UI']}/>
         </div>
     </div>
   )
