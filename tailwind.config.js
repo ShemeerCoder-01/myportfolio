1e5ae15c4fb8018631e453f83df969785f02bec2
Shemeer <shameerbem@gmail.com>
2023-11-14 20:55:29 +0530

    updated commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       animation:['motion-safe'],
+      screens:{
+        'lg':{min:'640px'}
+      }
     },
   },
   plugins: [],
